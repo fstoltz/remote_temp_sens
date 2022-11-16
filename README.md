f170158377bb36d60ba96acc1144c77d5debd86c
fstoltz <fredrik.stoltz@live.com>
2022-11-16 21:06:18 +0100

    added some code and made some folders

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # remote_temp_sens
+
+
+controller - node that sits in the pan room and turns off/on the pump
+
+
+sensor - senses the water temperature in the main building and transmits to the controller
---
+++ controller/controller/controller.ino
@@ -0,0 +1,60 @@
+
+// Imports related to Wifi Client
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
+
+
+// Imports related to Receiver radio module
+
+
+
+
+
+void setupWifi(){
+  
+}
+
+void setupRadio(){
+  
+}
+
+
+
+int turnOnRelay(){
+  
+}
+
+int turnOffRelay(){
+  
+}
+
+
+
+void setup() {
+
+  // Setup radio
+
+
+  // Setup Wifi client
+  
+}
+
+void loop() {
+  // Stand in infinite wait for a temp message from the radio module (i.e. polling mode)
+
+
+  // Temperature in main building is now known in variable houseTemp
+
+  /*
+   * if houseTemp < 45:
+   *    turnOnRelay();
+   * if houseTemp > 60:
+   *    turnOffRelay();
+   */
+
+  // Sleep for 1 minute
+  
+}
